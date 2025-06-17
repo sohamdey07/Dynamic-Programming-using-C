3bd3a70a8947be7be8fe8e84c903cf2eaf37faa9
sohamdey07 <sohamdey017@gmail.com>
2025-06-18 01:02:21 +0530

    knapsack updated

--- exam/knapsack.c
+++ exam/knapsack.c
@@ -1,105 +1,87 @@
 #include<stdio.h>
 
-float total_profit,x[50];
-
-void display(int n)
+void itemSort(float weight[],float profit[],float ratio[],int n)
 {
-    printf("The Solution vector will be:\n");
-    for(int i=0;i<n;i++)
-    {
-        printf("%f\t",x[i]);
-    }
-    printf("\n");
-    printf("Maximum profit: %f\n",total_profit);
-}
-
-// sort the item on the basis of p/w ratio
-void sort_decending(float weight[],float profit[],float ratio[],int n)
-{
-    int temp;
+    float temp;
     for(int i=0;i<n;i++)
     {
         for(int j=i+1;j<n;j++)
         {
             if(ratio[i]<ratio[j])
             {
-                temp=ratio[i];
-                ratio[i]=ratio[j];
-                ratio[j]=temp;
+                temp=ratio[j];
+                ratio[j]=ratio[i];
+                ratio[i]=temp;
 
-                temp=weight[i];
-                weight[i]=weight[j];
-                weight[j]=temp;
+                temp=weight[j];
+                weight[j]=weight[i];
+                weight[i]=temp;
 
-                temp=profit[i];
-                profit[i]=profit[j];
-                profit[j]=temp;
+                temp=profit[j];
+                profit[j]=profit[i];
+                profit[i]=temp;
             }
         }
     }
 }
 
-void greedy_knapsack(int n,float weight[],float profit[],float capacity)
+void optimal(float x[],int n,float maxprofit)
+{
+    printf("The result vector is:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%f\t",x[i]);
+    }
+    printf("\n");
+    printf("The maximum profit = %f\n",maxprofit);
+}
+
+void kanpsack(float weight[],float profit[],float x[],int n,int capacity)
 {
     int i;
+    float maxprofit=0.0;
     int u=capacity;
-    // initialize the solution vector
-    float ratio[20];
-
-    
-    for(i=0;i<n;i++)
-    {
-        ratio[i]=profit[i]/weight[i];
-    }
-
-    //initialise the solution vector by 0.0
-    for(i=0;i<n;i++)
-    {
-        x[i]=0.0;
-    }
-
-    //sort the items
-    sort_decending(weight,profit,ratio,n);
-
 
     for(i=0;i<n;i++)
     {
-        //if item's weight > remaining capacity then break from the loop
-        if(weight[i]>u)
+        if(weight[i]<u)
         {
-            break;
+            x[i]=1.0;
+            maxprofit+=profit[i];
+            u=u-weight[i];
         }
         else
         {
-            //if under capacity value the take as a whole
-            x[i]=1.0;
-            total_profit+=profit[i];
-            u=u-weight[i];
+            break;
         }
     }
-    //take the small portion /fractional of the item
-    if(i<n)
+    if((weight[i]>u) && u!=0)
     {
         x[i]=u/weight[i];
-        total_profit+=(profit[i]*x[i]); //fractional profit
+        maxprofit+=profit[i]*x[i];
     }
-    display(n);//display the vector and max profit
+    optimal(x,n,maxprofit);
 }
 
 void main()
 {
-    int n,capacity;
-    printf("Enter the number of jobs:");
+    int n;
+    printf("Enter the number of items:");
     scanf("%d",&n);
 
-    float weight[20],profit[20];
-    printf("Enter the items with their weight and profit:\n");
+    float weight[n],profit[n],ratio[n],x[n];
+    int capacity;
+
+    printf("Enter the jobs with their weights and profits :\n");
     for(int i=0;i<n;i++)
     {
-        printf("item no %d : ",i+1);
         scanf("%f %f",&weight[i],&profit[i]);
+        ratio[i]=profit[i]/weight[i];
+        x[i]=0.0;
     }
     printf("Enter the capacity:");
     scanf("%d",&capacity);
-    greedy_knapsack(n,weight,profit,capacity);
+    
+    itemSort(weight,profit,ratio,n);
+    kanpsack(weight,profit,x,n,capacity);
 }
